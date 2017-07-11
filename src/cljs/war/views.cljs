42599558d13d53933fb9740e59dfd1f752652db9
Andrew Beach <andrew@team.shoeboxed.com>
2017-07-11 08:49:53 -0400

    added in experimentation with draggables and droppables

--- src/cljs/war/views.cljs
+++ src/cljs/war/views.cljs
@@ -1,13 +1,14 @@
 (ns war.views
   (:require [re-frame.core :as re-frame]
             [re-com.core :as re-com]
-            [war.game-logic :as game]))
+            [war.game-logic :as game]
+            [reagent.core :as reagent]))
 
 (defn title []
   (let [name (re-frame/subscribe [:name])]
     (fn []
       [re-com/title
-       :label (str "Playing War!")
+       :label (str "Playing Cards...")
        :level :level1])))
 
 (defn counter [label & vals]
@@ -37,14 +38,52 @@
     [:img.deck {:src "images/png/back.png"
                 :on-click #(re-frame/dispatch [:update-decks (game/do-turn 15 [] @decks)])}]))
 
+(def pot-state (reagent/atom {:drop-area {:class "ui-widget-header"
+                                          :text "Drop here"}}))
+
+(defn card-did-mount [this]
+  (.draggable (js/$ (reagent/dom-node this))))
+
+(defn card-render [card]
+    [:div.ui-widget-content {:style {:width "138px"
+                                     :height "200px"
+                                     :border-radius "6px"}}
+     [:img.single-card {:style {:width "138px"
+                                :height "200px" }
+                        :src (game/card-filename card)}]])
+
+(defn card-view [card]
+  (reagent/create-class {:reagent-render card-render
+                         :component-did-mount card-did-mount}))
+
+(defn pot-did-mount [this]
+  (.droppable (js/$ (reagent/dom-node this))
+    #js {:drop (fn []
+                 (swap! pot-state assoc-in [:drop-area :class] "ui-widget-header ui-state-highlight")
+                 (swap! pot-state assoc-in [:drop-area :text] "Dropped!"))}))
+
+(defn pot-render []
+  (let [class (get-in @pot-state [:drop-area :class])
+        text (get-in @pot-state [:drop-area :text])]
+    [:div {:class class
+           :style {:width "500px"
+                   :height "300px"
+                   :padding "0.5em"
+                   :margin "10px"}}
+     [:p text]]))
+
+(defn pot-area []
+  (reagent/create-class {:reagent-render pot-render
+                         :component-did-mount pot-did-mount}))
+
 (defn game-board []
   (let [decks (re-frame/subscribe [:decks])]
     (fn []
       [re-com/h-box
        :width "100%"
        :children [[deck-view]
-                  [:img.single-card {:src (game/card-filename (peek (first @decks)))}]
-                  [:img.single-card {:src (game/card-filename (peek (second @decks)))}]
+                  [card-view (peek (first @decks))]
+                  [card-view (peek (second @decks))]
                   [deck-view]]]
       )))
 
@@ -60,6 +99,7 @@
      :height "100%"
      :children [[title]
                 [game-board]
+                [pot-area]
                 [counters]
                 [refresh-game]]]))
 
